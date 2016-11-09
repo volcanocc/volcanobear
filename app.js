a33532f020cdde90928b952ca861a8e262be8be1
CAN <canvolcano@foxmail.com>
2016-11-09 21:12:26 +0800

    react-router

--- app.js
+++ app.js
@@ -43,11 +43,6 @@ app.use('/logout', logout);
 app.use('*', routes);
 
 
-// app.get('/*', function(req, res) {
-//     res.sendfile('client/index.html');
-// });
-
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
